e61c3e9c2ec5bbdf0af2e2afc820df88e34f2208
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-10-04 12:42:50 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@
 # a copy of the GNU General Public License along with this program.
 # If not, see <http://www.gnu.org/licenses/>.
 
-version="0.4"
+version="0.4.1"
 
 # Allows timeout when launched vai 'Run in Terminal'
 function gerror() { sleep 3; exit 1; }
