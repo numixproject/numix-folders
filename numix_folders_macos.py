fb14833dae598be4f80ebeb72afd12823766b1f6
Benjamin Krajancic <ben.k99@outlook.com.au>
2020-11-03 09:01:16 +1100

    Code cleanup

--- numix_folders_macos.py
+++ numix_folders_macos.py
@@ -10,44 +10,37 @@ def _update():
         for name in files:
             if name.endswith(".svg"):
                 source = "/".join(pathlib.Path(root).parts)
-                filename = name
-                dest = pathlib.Path("out") / source
-                os.makedirs(dest, exist_ok=True)
+                out_path = pathlib.Path("out") / source
+                if ("64" not in out_path.parts and
+                        "scalable" not in out_path.parts):
+                    continue
 
-                out_path = dest
-                scalable = "scalable" in pathlib.Path(root).parts
-                output_width = (str(pathlib.Path(root).parts[-2])
-                                if not scalable else "64")
-                output_height = (str(pathlib.Path(root).parts[-2])
-                                 if not scalable else "64")
+                filename = name
+                os.makedirs(out_path, exist_ok=True)
+
                 png_name = filename.replace(".svg", ".png")
                 png_path = str((out_path / png_name).absolute())
 
                 if os.path.exists(png_path):
                     os.remove(png_path)
 
-                if "64" in out_path.parts or "scalable" in out_path.parts:
-                    png_path_128 = os.path.sep.join(out_path.parts)
-                    png_path_128 = png_path_128.replace("64", "128")
-                    os.makedirs(str(png_path_128), exist_ok=True)
-                    png_path_128 = os.path.join(png_path_128, png_name)
-                    cairosvg.svg2png(
-                        url=str(pathlib.Path(source) / filename),
-                        write_to=png_path_128,
-                        output_width=128,
-                        output_height=128
-                    )
+                png_path_128 = os.path.sep.join(out_path.parts)
+                png_path_128 = png_path_128.replace("64", "128")
 
-                    ico_path = png_path_128.replace(".png", ".ico")
-                    call(["png2icns", ico_path, png_path_128],
-                         stdout=PIPE, stderr=PIPE)
+                os.makedirs(str(png_path_128), exist_ok=True)
 
+                png_path_128 = os.path.join(png_path_128, png_name)
                 cairosvg.svg2png(
                     url=str(pathlib.Path(source) / filename),
-                    write_to=png_path,
-                    output_width=int(output_width),
-                    output_height=int(output_height)
+                    write_to=png_path_128,
+                    output_width=128,
+                    output_height=128
                 )
+
+                ico_path = png_path_128.replace(".png", ".ico")
+                call(["png2icns", ico_path, png_path_128],
+                     stdout=PIPE, stderr=PIPE)
+                os.remove(png_path_128)
 
 
 if __name__ == "__main__":
